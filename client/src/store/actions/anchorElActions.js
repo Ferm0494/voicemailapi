d455a90521efa8717cdd2bb2c8ce9513d304eade
Ferm0494 <ferm0494@gmail.com>
2020-08-13 21:14:04 -0600

    Fetch and made other endpoint on api on react/node

--- client/src/store/actions/anchorElActions.js
+++ client/src/store/actions/anchorElActions.js
@@ -1,3 +1,4 @@
+import fetch from "node-fetch"
 
 const SHOW_ANCHOR = "SHOW_ANCHOR"
 const HIDE_ANCHOR = "HIDE_ANCHOR"
@@ -6,6 +7,18 @@ const show_anchor=(event)=>{
      return {type: SHOW_ANCHOR, payload: event}
 }
 const hide_anchor = async(folder,id)=>{
+
+    
+    let response = await fetch('/messages',{
+        method: "PATCH",
+        headers:{
+            'Content-Type':'application/json',
+        },
+        body: JSON.stringify({folder})
+    })
+
+    const body = await response.text();
+    console.log(body)
 
    return  {type: HIDE_ANCHOR, payload:null}
 }
